c550ff8cc3c8a87826e289ab1a8a34f8a3fa4967
RAguina <rodrigoaguinagalde@gmail.com>
2023-12-11 19:18:30 -0300

    v0.51d

--- src/api/inmuebles.js
+++ src/api/inmuebles.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000/inmuebles"
+//const API_URL = "http://localhost:3000/inmuebles"
+const API_URL = "https://rhapi-dev-kkbb.3.us-1.fl0.io/inmuebles"
+
 export const registrarInmueble = async (inmueble) => {
   try {
     const response = await axios.post(API_URL, {inmueble},{headers: {'Authorization': `Bearer ${token}`}});
@@ -9,11 +11,11 @@ export const registrarInmueble = async (inmueble) => {
     console.error('Error registrando el inmueble', error.response.data);
     throw error;
   }
-};
+};  
 
 export const actualizarInmueble = async (id, inmuebleModificado) => {
   try {
-    const response = await axios.put(`http://localhost:3000/inmuebles/${id}`, inmuebleModificado);
+    const response = await axios.put(`https://rhapi-dev-kkbb.3.us-1.fl0.io/inmuebles/${id}`, inmuebleModificado);
     return response.data;
   } catch (error) {
     console.error(`Error actualizando el inmueble con id ${id}`, error);
@@ -23,7 +25,7 @@ export const actualizarInmueble = async (id, inmuebleModificado) => {
 
 export const eliminarInmueble = async (id) => {
   try {
-    const response = await axios.delete(`http://localhost:3000/inmuebles/${id}`);
+    const response = await axios.delete(`https://rhapi-dev-kkbb.3.us-1.fl0.io/inmuebles/${id}`);
     return response.data;
   } catch (error) {
     console.error(`Error eliminando el inmueble con id ${id}`, error);
@@ -33,7 +35,8 @@ export const eliminarInmueble = async (id) => {
 
 export const obtenerInmuebles = async () => {
   try {
-    const response = await axios.get('http://localhost:3000/inmuebles');
+    const response = await axios.get(API_URL);
+    //const response = await axios.get(`${process.env.REACT_APP_API_URL_LOCAL}/inmuebles`);
     return response.data;
   } catch (error) {
     console.error('Error obteniendo los inmuebles', error);
@@ -44,7 +47,7 @@ export const obtenerInmuebles = async () => {
 
 export const obtenerInmueblePorId = async (id) => {
   try {
-    const response = await axios.get(`http://localhost:3000/inmuebles/${id}`);
+    const response = await axios.get(`https://rhapi-dev-kkbb.3.us-1.fl0.io/inmuebles/${id}`);
     return response.data;
   } catch (error) {
     console.error(`Error obteniendo el inmueble con id ${id}`, error);
