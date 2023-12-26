3dbc7583d337639855bcf8cd5e96d70c4647b8b5
RAguina <rodrigoaguinagalde@gmail.com>
2023-12-26 08:53:37 -0300

    v0.57.8

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,18 +1,33 @@
 // ProtectedRoute.jsx
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
+import ErrorPage from '../components/ErrorPage';
 
 const ProtectedRoute = ({ element, roles }) => {
   // Simulación del estado de autenticación y roles (ajusta según tu implementación real)
   const isAuthenticated = true; // Asegúrate de reemplazar con tu lógica real de autenticación
   const userRoles = ['admin']; // Asegúrate de reemplazar con tus roles reales
 
+  const [showError, setShowError] = useState(!isAuthenticated);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      const timer = setTimeout(() => {
+        setShowError(false);
+      }, 5000); // Oculta ErrorPage después de 5 segundos
+      return () => clearTimeout(timer);
+    }
+  }, [isAuthenticated]);
+
   if (isAuthenticated && roles.every((role) => userRoles.includes(role))) {
     return <Route element={element} />;
+  } else if(showError){
+    return <ErrorPage mensaje="No estás autenticado. Serás redirigido a la página de inicio de sesión en unos segundos." tipo="error" />;
   } else {
-    // Puedes redirigir a una página de inicio de sesión o mostrar un mensaje de acceso denegado
-    return <Navigate to="/" />;
+    return <Navigate to="/login" />;
   }
 };
 
 export default ProtectedRoute;
+
+//    <ErrorPage mensaje={mensaje.texto} tipo={mensaje.tipo} />
