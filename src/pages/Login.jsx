c3abd9fd23c247a6a93387ce94a4e21e68a3655d
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-07 18:23:13 -0300

    v0.61.4

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -72,7 +72,7 @@ function Login() {
       </div>
       <div className="flex items-center justify-between">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-          Iniciar sesión
+          Confirmar
         </button>
       </div>
     </form>
