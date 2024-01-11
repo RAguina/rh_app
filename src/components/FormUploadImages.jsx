8b1ceb361ca78f88a322231d398c5b04491c3288
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-10 21:02:03 -0300

    v0.62.5

--- src/components/FormUploadImages.jsx
+++ src/components/FormUploadImages.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDropzone } from 'react-dropzone'; // Importa useDropzone de react-dropzone
 import axios from 'axios'; // Importa axios
+import { subirImagen } from '../api/imagenes_inmuebles';
 
 const FormUploadImages = () => {
   const { getRootProps, getInputProps } = useDropzone({
@@ -10,21 +11,9 @@ const FormUploadImages = () => {
       // Itera sobre cada archivo aceptado
       for (const file of acceptedFiles) {
         try {
-          // Crea un objeto FormData
-          const formData = new FormData();
-          formData.append('image', file);
-          formData.append('propietario_id', idPropietario); 
-
-          // Realiza la solicitud POST al servidor
-          const response = await axios.post("https://rhapi-dev-kkbb.3.us-1.fl0.io/imagen_inmuebles/upload", formData, {
-              headers: {
-              'Content-Type': 'multipart/form-data',
-              'Authorization': `Bearer ${localStorage.getItem('token')}`, // Token JWT
-            },
-          });
-          console.log("El estado de la respuesta es:", response.status)
-          console.log('Respuesta del servidor:', response.data.propiedad_id); 
-          console.log("response.data.public_id",response.data.nombre_imagen);
+          // Llama a subirImagen para cada archivo
+          const response = await subirImagen(file, idPropietario);
+          console.log('Respuesta del servidor:', response);
         } catch (error) {
           console.log("Error subiendo imagen");
           throw error;
