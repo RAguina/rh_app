776b3c19386fce758e692b4d88136662b0a75e60
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-11 09:49:25 -0300

    v0.62.6

--- src/api/imagenes_inmuebles.js
+++ src/api/imagenes_inmuebles.js
@@ -1,5 +1,41 @@
 import axios from "axios";
 const API_URL = "https://rhapi-dev-kkbb.3.us-1.fl0.io"
+
+
+export const subirImagen = async (files, idPropietario) => {
+  try {
+    // Crea un objeto FormData
+    const formData = new FormData();
+
+    // Itera sobre cada archivo y añádelo al objeto FormData
+    files.forEach((file, index) => {
+      formData.append('image' + index, file);
+    });
+
+    formData.append('propietario_id', idPropietario); 
+
+    // Realiza la solicitud POST al servidor
+    const response = await axios.post("https://rhapi-dev-kkbb.3.us-1.fl0.io/imagen_inmuebles/upload", formData, {
+        headers: {
+        'Content-Type': 'multipart/form-data'
+        , 'Authorization': `Bearer ${localStorage.getItem('token')}`, // Token JWT
+      },
+    });
+    console.log("El estado de la respuesta es:", response.status)
+    console.log('Respuesta del servidor:', response.data.propiedad_id); 
+    console.log("response.data.public_id",response.data.nombre_imagen);
+    return response.data;
+  } catch (error) {
+    console.log("Error subiendo imagen 25");
+    //console.error('Error subiendo la imagen', error.response ? error.response.data : error.message);
+    throw error;
+  }
+};
+
+
+
+
+/* Vieja subirImagen
 export const subirImagen = async (file,idPropietario) => {
   try {
     // Crea un objeto FormData
@@ -24,7 +60,7 @@ export const subirImagen = async (file,idPropietario) => {
     throw error;
   }
 };
-
+*/
 
 
 
--- src/config/AuthContext.jsx
+++ src/config/AuthContext.jsx
@@ -4,21 +4,22 @@
 
   export const AuthProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+    const [idPropietario, setIdPropietario] = useState(null);
 
     useEffect(() => {
       const token = localStorage.getItem('token');
       if (token) {
         setIsLoggedIn(true);
+        const decodedToken = jwt.decode(token);
+        setIdPropietario(decodedToken.idPropietario);
       }
     }, []);
-
-    useEffect(() => {
-      console.log("isLoggedIn ha cambiado a:", isLoggedIn);
-    }, [isLoggedIn]);
 
     const handleLoginSuccess = (newToken, expiry) => {
       console.log('handleLoginSuccess llamado');
       localStorage.setItem('token', JSON.stringify({ token: newToken, expiry }));
+      const decodedToken = jwt.decode(newToken);
+      setIdPropietario(decodedToken.idPropietario)
       setIsLoggedIn(true);
       return newToken;
     };
@@ -27,10 +28,11 @@
     const handleLogout = () => {
       localStorage.removeItem('token');
       setIsLoggedIn(false);
+      setIdPropietario(null);
     };
 
     return (
-      <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, handleLoginSuccess, handleLogout }}>
+      <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, idPropietario, handleLoginSuccess, handleLogout }}>
         {children}
       </AuthContext.Provider>
     );
