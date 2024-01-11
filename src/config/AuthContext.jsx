297e21860e7767c77e2fd4a45c34f8bc895c64ca
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-11 12:33:25 -0300

    v0.63.2

--- src/config/AuthContext.jsx
+++ src/config/AuthContext.jsx
@@ -1,45 +1,42 @@
-  import React, { createContext, useState, useEffect } from 'react';
-  import jwt from 'jsonwebtoken';
+import React, { createContext, useState, useEffect } from 'react';
+import jwtDecode from 'jwt-decode'; // Importa jwt-decode
 
-  export const AuthContext = createContext();
+export const AuthContext = createContext();
 
-  export const AuthProvider = ({ children }) => {
-    console.log("console log 1");
-    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
-    const [idPropietario, setIdPropietario] = useState(null);
-    console.log("console log 2");
-    useEffect(() => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        setIsLoggedIn(true);
-        const decodedToken = jwt.decode(token);
-        if (decodedToken) {
-          setIdPropietario(decodedToken.idPropietario);
-        }
-      }
-    }, []);
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+  const [idPropietario, setIdPropietario] = useState(null);
 
-    const handleLoginSuccess = (newToken, expiry) => {
-      console.log('handleLoginSuccess llamado');
-      localStorage.setItem('token', JSON.stringify({ token: newToken, expiry }));
-      const decodedToken = jwt.decode(newToken);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+      const decodedToken = jwtDecode(token); // Usa jwt-decode para decodificar el token
       if (decodedToken) {
         setIdPropietario(decodedToken.idPropietario);
       }
-      setIsLoggedIn(true);
-      return newToken;
-    };
-    
+    }
+  }, []);
 
-    const handleLogout = () => {
-      localStorage.removeItem('token');
-      setIsLoggedIn(false);
-      setIdPropietario(null);
-    };
-
-    return (
-      <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, idPropietario, handleLoginSuccess, handleLogout }}>
-        {children}
-      </AuthContext.Provider>
-    );
+  const handleLoginSuccess = (newToken, expiry) => {
+    localStorage.setItem('token', JSON.stringify({ token: newToken, expiry }));
+    const decodedToken = jwtDecode(newToken); // Usa jwt-decode para decodificar el token
+    if (decodedToken) {
+      setIdPropietario(decodedToken.idPropietario);
+    }
+    setIsLoggedIn(true);
+    return newToken;
   };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    setIdPropietario(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, idPropietario, handleLoginSuccess, handleLogout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
