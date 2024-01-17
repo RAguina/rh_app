6b5b3443c140d05ab55789e2c3c24822f22b5e76
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-16 23:27:54 -0300

    v0.66.6l

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { AuthProvider } from "../config/AuthContext";
 
 function Header() {
     return(
-        <div className="flex from-blue-200 to-blue-600 bg-gradient-to-br">
+        <div className="flex from-blue-200 to-blue-600 bg-gradient-to-tl">
         <div className="w-2/5 flex flex-col items-center justify-center">
             <img src={logo} alt="logo" className="w-36 h-36"/>
             <h1 className="text-4xl font-bold">Necochea Rent House</h1>
