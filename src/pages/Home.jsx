2929120f0b053c24886672ea5743542c3fe8c91e
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-17 21:57:59 -0300

    v0.66.6v

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 
 const Home = () => {
   return (
-    <div className="">
+    <>
+    <div className="w-1/2">
       <p className='text-5xl text-center mt-10 mb-10 font-bold underline'>Noticias</p>
-      <article className="flex mb-10 gap-5 w-1/2">
+      <article className="flex mb-10 gap-5">
         <img src="/news1.jpg" alt="Imagen artículo 1" className="w-1/2 rounded-xl ml-5"/>
         <div className="w-1/2">
           <h2 className='text-4xl'>Cae la demanda de alquileres por los altos precios</h2>
@@ -18,6 +19,10 @@ const Home = () => {
         <img src="/news2.jpg" alt="Imagen artículo 2" className="w-1/2 rounded-xl ml-5"/>
       </article>
     </div>
+    <div>
+      <h1>Anuncia tu propiedad aqui!</h1>
+    </div>
+    </>
   )
 }
 
