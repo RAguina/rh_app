292868fcc5a172325cfcd0467ffb29e758cc718d
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-17 22:10:37 -0300

    v0.66.6w

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,27 @@
-
 const Home = () => {
   return (
-    <>
-    <div className="w-1/2">
-      <p className='text-5xl text-center mt-10 mb-10 font-bold underline'>Noticias</p>
-      <article className="flex mb-10 gap-5">
-        <img src="/news1.jpg" alt="Imagen artículo 1" className="w-1/2 rounded-xl ml-5"/>
-        <div className="w-1/2">
-          <h2 className='text-4xl'>Cae la demanda de alquileres por los altos precios</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-        </div>
-      </article>
-      <article className="flex ml-5 mb-10 gap-5 w-1/2">
-        <div className="w-1/2">
-          <h2 className='text-4xl'>Visita las Termas del Campo en Necochae</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-        </div>
-        <img src="/news2.jpg" alt="Imagen artículo 2" className="w-1/2 rounded-xl ml-5"/>
-      </article>
+    <div className="flex">
+      <div className="w-1/2">
+        <p className='text-5xl text-center mt-10 mb-10 font-bold underline'>Noticias</p>
+        <article className="flex mb-10 gap-5">
+          <img src="/news1.jpg" alt="Imagen artículo 1" className="w-1/2 rounded-xl ml-5"/>
+          <div className="w-1/2">
+            <h2 className='text-4xl'>Cae la demanda de alquileres por los altos precios</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          </div>
+        </article>
+        <article className="flex ml-5 mb-10 gap-5 w-1/2">
+          <div className="w-1/2">
+            <h2 className='text-4xl'>Visita las Termas del Campo en Necochae</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          </div>
+          <img src="/news2.jpg" alt="Imagen artículo 2" className="w-1/2 rounded-xl ml-5"/>
+        </article>
+      </div>
+      <div className="w-1/2">
+        <h1>Anuncia tu propiedad aqui!</h1>
+      </div>
     </div>
-    <div>
-      <h1>Anuncia tu propiedad aqui!</h1>
-    </div>
-    </>
   )
 }
 
