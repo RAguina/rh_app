9385f117e0551c760bfe88e39069ba7c43cbec89
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-19 19:31:00 -0300

    v0.67.8c

--- src/components/DetalleInmueble.jsx
+++ src/components/DetalleInmueble.jsx
@@ -15,7 +15,6 @@ const DetalleInmueble = () => {
       try {
         const respuesta = await axios.get(`${API_URL_INMUEBLES}/${id}`);
         setInmueble(respuesta.data);
-        console.log("inmueble.fecha",inmueble.createdAt);
       } catch (error) {
         console.error('Hubo un error al obtener el inmueble: ', error);
       }
@@ -49,7 +48,7 @@ const DetalleInmueble = () => {
       <p className="mb-2">Precio: <span className='font-bold'>${inmueble.precio_propiedad}</span></p>
       <p className="mb-2">Estado: <span className='font-bold'>{inmueble.estado_propiedad}</span></p>
       <p className="mb-2">ID del propietario: <span className='font-bold'>{inmueble.propietario_id}</span></p>
-      <p className="mb-2">Fecha de subida: <span className='font-bold'>{new Date(inmueble.createdAt).toLocaleDateString()}</span></p>
+      <p className="mb-2">Fecha de subida: <span className='font-bold'>{inmueble && inmueble.createdAt ? new Date(inmueble.createdAt).toLocaleDateString() : 'N/A'}</span></p>
       <div className="grid grid-cols-2 gap-4">
       {imagenes.map((imagen, index) => (
         <img key={index} src={imagen.url_imagen} alt={`Imagen ${index + 1}`} className="w-full object-cover h-64" />
