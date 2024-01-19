e45f5b715338014c992b8a9b1a96b230edec44a0
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-18 21:27:30 -0300

    v0.66.8e

--- src/components/FormUploadImages.jsx
+++ src/components/FormUploadImages.jsx
@@ -32,12 +32,15 @@ const FormUploadImages = () => {
   });
   
   return (
+    <>
+    <NavBarLine propiedadId={propiedadId} />
     <div className='grid'>
       <div className='min-h-50vh' {...getRootProps()}>
         <input {...getInputProps()} />
         <p className='text-blue-500 mt-10 text-2xl text-center font-bold'>Arrastra y suelta algunas imágenes aquí, o haz clic para seleccionar imágenes</p>
       </div>
     </div>
+    </>
   )
 };
 
