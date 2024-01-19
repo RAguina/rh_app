048de592eff8f004bd12cc2186ecfd757ba67720
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-19 14:46:37 -0300

    v0.67.2e

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
 import Nav from "./Nav";
 import logo from "../assets/logonr.svg"
-import { AuthProvider } from "../config/AuthContext";
+
 
 function Header() {
-    return(
-        <div className="flex from-blue-200 to-blue-600 bg-gradient-to-tl">
+  const [isNavOpaque, setIsNavOpaque] = useState(false);
+  return(
+      <div className="flex from-blue-200 to-blue-600 bg-gradient-to-tl">
         <div className="w-2/5 flex flex-col items-center justify-center">
-            <img src={logo} alt="logo" className="w-36 h-36"/>
-            <h1 className="text-4xl font-bold">Necochea Rent House</h1>
-            <span className="font-bold">Alquileres Temporarios</span> 
-        </div>
-        <div className="w-3/5">
-            <Nav/>
-        </div>
-    </div>
-    )
+        <img src={logo} alt="logo" className="w-36 h-36"/>
+        <h1 className="text-4xl font-bold">Necochea Rent House</h1>
+        <span className="font-bold">Alquileres Temporarios</span> 
+      </div>
+      <div className="w-3/5">
+        <Nav isOpaque={isNavOpaque} />
+        <button onClick={() => setIsNavOpaque(!isNavOpaque)}>Que diseño choto, lo podes cambiar?</button>      
+      </div>
+  </div>
+  )
 }
 
 export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,78 +2,75 @@ import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../config/AuthContext';
 
-const Nav = () => {
+const Nav = ({ isOpaque}) => {
 
-    const isAdmin = true;
-    const { isLoggedIn, handleLogout} = useContext(AuthContext);
+  const isAdmin = true;
+  const { isLoggedIn, handleLogout} = useContext(AuthContext);
 
-    return (
-        
-        <nav className="flex flex-wrap justify-center mt-10 gap-2">
-            <Link 
-                to="/" 
-                className={`navLinks ${location.pathname === '/' ? 'active' : ''}`}
-            >Inicio</Link>
+  return (
+      
+      <nav className={`flex flex-wrap justify-center mt-10 gap-2 ${isOpaque ? 'ocultar' : ''}`}>
+          <Link 
+              to="/" 
+              className={`navLinks ${location.pathname === '/' ? 'active' : ''}`}
+          >Inicio</Link>
 
-            <Link 
-                to="/registrarPropiedad" 
-                className={`navLinks ${location.pathname === '/registrarPropiedad' ? 'active' : ''}`}
-            >Registrar Propiedad</Link>
+          <Link 
+              to="/registrarPropiedad" 
+              className={`navLinks ${location.pathname === '/registrarPropiedad' ? 'active' : ''}`}
+          >Registrar Propiedad</Link>
 
-            <Link 
-                to="/listadoPropiedades" 
-                className={`navLinks ${location.pathname === '/listadoPropiedades' ? 'active' : ''}`}
-            >Listado Propiedades</Link>
+          <Link 
+              to="/listadoPropiedades" 
+              className={`navLinks ${location.pathname === '/listadoPropiedades' ? 'active' : ''}`}
+          >Listado Propiedades</Link>
 
-            <Link 
-                to="/queHacer" 
-                className={`navLinks ${location.pathname === '/queHacer' ? 'active' : ''}`}
-            >Que hacer en Necochea?</Link>
+          <Link 
+              to="/queHacer" 
+              className={`navLinks ${location.pathname === '/queHacer' ? 'active' : ''}`}
+          >Que hacer en Necochea?</Link>
 
-            <Link 
-            to="/registrarUsuario" 
-            className={`navLinks ${location.pathname === '/registrarUsuario' ? 'active' : ''}`}
-            >Registrarse</Link>
+          <Link 
+          to="/registrarUsuario" 
+          className={`navLinks ${location.pathname === '/registrarUsuario' ? 'active' : ''}`}
+          >Registrarse</Link>
 
-            <Link 
-            to="/contact" 
-            className={`navLinks ${location.pathname === '/contact' ? 'active' : ''}`}
-            >Contactanos</Link>
-            
-            {isLoggedIn ? (
-                <>
-                    <Link 
-                        to="/" 
-                        onClick={handleLogout}
-                        className={`navLinks2`}
-                    >Cerrar Sesión</Link>
-                    <Link 
-                        to="/formUploadImages" 
-                        className={`navLinks2 ${location.pathname === '/formUploadImages' ? 'active' : ''}`}
-                    >Subir Imagenes</Link>
-                </>
-            ) : (
-                <Link 
-                    to="/login" 
-                    className={`navLinks2 ${location.pathname === '/login' ? 'active' : ''}`}
-                >Iniciar Sesión</Link>
-            )}
+          <Link 
+          to="/contact" 
+          className={`navLinks ${location.pathname === '/contact' ? 'active' : ''}`}
+          >Contactanos</Link>
+          
+          {isLoggedIn ? (
+              <>
+                  <Link 
+                      to="/" 
+                      onClick={handleLogout}
+                      className={`navLinks2`}
+                  >Cerrar Sesión</Link>
+                  <Link 
+                      to="/formUploadImages" 
+                      className={`navLinks2 ${location.pathname === '/formUploadImages' ? 'active' : ''}`}
+                  >Subir Imagenes</Link>
+              </>
+          ) : (
+              <Link 
+                  to="/login" 
+                  className={`navLinks2 ${location.pathname === '/login' ? 'active' : ''}`}
+              >Iniciar Sesión</Link>
+          )}
 
-            {isAdmin && (
-                <Link
-                to="/adminPanel"
-                className={`navLinks2 ${
-                    location.pathname === '/adminPanel' ? 'active' : ''
-                }`}
-                >
-                Admin Panel
-                </Link>
-            )}
-
-            
-        </nav>
-
-    )
+          {isAdmin && (
+              <Link
+              to="/adminPanel"
+              className={`navLinks2 ${
+                  location.pathname === '/adminPanel' ? 'active' : ''
+              }`}
+              >
+              Admin Panel
+              </Link>
+          )}
+      </nav>
+   )
 }
 
 export default Nav;
