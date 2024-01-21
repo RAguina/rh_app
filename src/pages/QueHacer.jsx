4423f583c7274775ac8dd0a43b0861ee831a4bc3
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-21 00:41:48 -0300

    v0.68.6b

--- src/pages/QueHacer.jsx
+++ src/pages/QueHacer.jsx
@@ -5,7 +5,7 @@ function QueHacer() {
     <div className='min-h-screen'>
       <h1 className='text-3xl mt-10 mb-20 text-center font-bold'>QueHacer En Necochea?</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <CardQueHacer imagen="/pesca1.jpg" titulo="Pesca de Mar" descripcion="Descripción de la actividad de pesca." />
+        <CardQueHacer imagen="/pesca1.jpg" titulo="Pesca de Mar" descripcion="Descripción de la actividad de pesca. adasdasdasdasdasdsadasdasdas" />
         <CardQueHacer imagen="/quehacer3.jpg" titulo="El Point" descripcion="Restaurante bar en el medio del parque Miguel lillo y la playa, ideal para estar en familia y amigos" />
         <CardQueHacer imagen="/quehacer2.jpg" titulo="Playas" descripcion="Las playas mas anchas, las minas mas lindas del mundo, locatti, barreda" />
         <CardQueHacer imagen="/pesca2.jpg" titulo="Pesca de Rio" descripcion="Descripción de la actividad de pesca." />
