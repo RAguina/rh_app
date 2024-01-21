1a7b3df1980f499c0d3d6390ae0c4542a988d8b5
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-20 23:49:07 -0300

    v0.68.2d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,32 @@
 const Home = () => {
   return (
     <div className="flex flex-col sm:flex-row">
-      <div className="w-1/2">
+      <div className="w-full sm:w-1/2">
         <p className='text-5xl text-center mt-10 mb-10 font-bold underline'>Noticias</p>
-        <article className="flex mb-10 gap-5">
-          <img src="/news1.jpg" alt="Imagen artículo 1" className="w-1/2 rounded-xl ml-5"/>
-          <div className="w-1/2">
+        <div className="flex flex-col sm:flex-row mb-10 gap-5">
+          <div className="w-full sm:w-1/2">
+            <img src="/news1.jpg" alt="Imagen artículo 1" className="w-full h-64 object-cover rounded-xl"/>
+          </div>
+          <article className="w-full sm:w-1/2">
             <h2 className='text-4xl'>Cae la demanda de alquileres por los altos precios</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-          </div>
-        </article>
-        <article className="flex ml-5 mb-10 gap-5">
-          <div className="w-1/2">
+          </article>
+        </div>
+        <div className="flex flex-col sm:flex-row ml-5 mb-10 gap-5">
+          <article className="w-full sm:w-1/2">
             <h2 className='text-4xl'>Visita las Termas del Campo en Necochae</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          </article>
+          <div className="w-full sm:w-1/2">
+            <img src="/news2.jpg" alt="Imagen artículo 2" className="w-full h-64 object-cover rounded-xl"/>
           </div>
-          <img src="/news2.jpg" alt="Imagen artículo 2" className="w-1/2 rounded-xl ml-5"/>
-        </article>
+        </div>
       </div>
-      <div className="w-1/2 border">
+      <div className="w-full sm:w-1/2 border">
         <h1 className='text-5xl text-center mt-10 mb-10 font-bold underline'>Anuncia tu propiedad aqui!</h1>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
