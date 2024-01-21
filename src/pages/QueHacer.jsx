6ee5799babeca599784ee793961e7a6ef18d2443
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-21 00:26:58 -0300

    v0.68.5

--- src/pages/QueHacer.jsx
+++ src/pages/QueHacer.jsx
@@ -3,7 +3,7 @@ import CardQueHacer from '../components/CardQueHacer';
 function QueHacer() {
   return (
     <div className='min-h-screen'>
-      <h1 className='text-3xl mt-10 mb-20 text-center font-bold'>QueHacer En Necochea?</h1>
+      <h1 className='text-3xl mt-10 mb-20 text-center font-bold w-full'>QueHacer En Necochea?</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <CardQueHacer imagen="/pesca1.jpg" titulo="Pesca de Mar" descripcion="Descripción de la actividad de pesca." />
         <CardQueHacer imagen="/quehacer3.jpg" titulo="El Point" descripcion="Restaurante bar en el medio del parque Miguel lillo y la playa, ideal para estar en familia y amigos" />
