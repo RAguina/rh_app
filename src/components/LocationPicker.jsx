7500226044040b13bcb88e2a0c56c552c189030f
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-22 17:36:18 -0300

    v0.69.9f

--- src/components/LocationPicker.jsx
+++ src/components/LocationPicker.jsx
@@ -1,37 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet'; // Importa Leaflet para poder acceder a la función invalidateSize
 
 const LocationPicker = ({ onLocationSelect }) => {
-  const [map, setMap] = useState(null);
+  const defaultCenter = [-38.5545, -58.7396];
 
   const handleLocationSelect = (e) => {
     onLocationSelect(e.latlng);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      // Verifica si el mapa existe antes de intentar recargarlo
-      if (map) {
-        // Invalida el tamaño del mapa para forzar una recarga
-        map.invalidateSize();
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [map]);
-
   return (
-    <MapContainer center={[-38.5545, -58.7396]} zoom={13} style={{ height: "100vh", width: "100%" }} onClick={handleLocationSelect} whenCreated={setMap}>
+    <MapContainer center={defaultCenter} zoom={13} style={{ height: "500px", width: "100%" }} onClick={handleLocationSelect}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[-38.5545, -58.7396]}>
+      <Marker position={defaultCenter}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
