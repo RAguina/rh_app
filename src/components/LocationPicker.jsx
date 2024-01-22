a082494c68406417b6dde54e8e28e64413c03a8c
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-22 17:29:51 -0300

    v0.69.9e

--- src/components/LocationPicker.jsx
+++ src/components/LocationPicker.jsx
@@ -1,9 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'; // Importa Leaflet para poder acceder a la función invalidateSize
 
 const LocationPicker = ({ onLocationSelect }) => {
+  const [map, setMap] = useState(null);
+
+  const handleLocationSelect = (e) => {
+    onLocationSelect(e.latlng);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Verifica si el mapa existe antes de intentar recargarlo
+      if (map) {
+        // Invalida el tamaño del mapa para forzar una recarga
+        map.invalidateSize();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [map]);
+
   return (
-    <MapContainer center={[-38.5545, -58.7396]} zoom={13} style={{ height: "500px", width: "100%" }} onClick={(e) => onLocationSelect(e.latlng)}>
-      <ZoomControl position="topright" />
+    <MapContainer center={[-38.5545, -58.7396]} zoom={13} style={{ height: "100vh", width: "100%" }} onClick={handleLocationSelect} whenCreated={setMap}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -16,6 +39,5 @@ const LocationPicker = ({ onLocationSelect }) => {
     </MapContainer>
   );
 };
-
 
 export default LocationPicker;
