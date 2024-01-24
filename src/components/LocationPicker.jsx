a75ba4ebbfeafdd3fc47767a2cbed0e16a5250a3
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-24 14:21:23 -0300

    v0.72.0

--- src/components/LocationPicker.jsx
+++ src/components/LocationPicker.jsx
@@ -2,35 +2,17 @@ import React, { useState, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const LocationPicker = ({ onLocationSelect }) => {
-  /* New York
-  const [viewport, setViewport] = useState({
-    latitude: 40.7128, // Nueva York
-    longitude: -74.0060, // Nueva York
-    zoom: 13,
-    width: '100%',
-    height: '500px',
-    center: [-74.0060, 40.7128], // Nueva York
-  });
-  */
-  
-  // /* Necochea
   const [viewport, setViewport] = useState({
     latitude: -38.5545,
     longitude: -58.7396,
-    zoom: 14,
+    zoom: 13,
     width: '100%',
     height: '500px',
     center: [-58.7396, -38.5545],
   });
-  
+
   useEffect(() => {
     mapboxgl.accessToken = import.meta.env.VITE_REACT_APP_MAPBOX_TOKEN;
-
-    // Configuración detallada de Mapbox GL JS
-    mapboxgl.config.API_URL = 'https://api.mapbox.com';
-    mapboxgl.config.FAIL_IF_MAJOR_PERFORMANCE_CAVEAT = false;
-    mapboxgl.config.WEBGL_DEBUG_CANVAS_CONTEXT = true;
-    mapboxgl.config.WEBGL_DEBUG_SHADERS = true;
 
     const map = new mapboxgl.Map({
       container: 'map-container',
@@ -39,22 +21,26 @@ const LocationPicker = ({ onLocationSelect }) => {
       zoom: viewport.zoom,
     });
 
+    const marker = new mapboxgl.Marker(); // Crea un marcador
+
     map.on('load', () => {
       console.log('Mapa cargado exitosamente.');
     });
 
     map.on('click', (e) => {
-      onLocationSelect({ lat: e.lngLat.lat, lng: e.lngLat.lng });
+      const { lng, lat } = e.lngLat;
+      onLocationSelect({ lat, lng });
+
+      // Mueve el marcador a la nueva ubicación
+      marker.setLngLat([lng, lat]).addTo(map);
     });
 
     return () => {
       map.remove();
     };
   }, [onLocationSelect, viewport]);
-  
-  return <div id="map-container" style={{ width: '500px', height: '500px', border: '1px solid red' }} />;
 
-  //return <div id="map-container" style={{ width: viewport.width, height: viewport.height, border: '1px solid red' }} />;
+  return <div id="map-container" style={{ width: '100%', height: '500px', border: '1px solid red' }} />;
 };
 
 export default LocationPicker;
