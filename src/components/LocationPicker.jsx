78b7a5ed23aa12334e2f978703e5d97c749f3755
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-24 14:30:27 -0300

    v0.72.1

--- src/components/LocationPicker.jsx
+++ src/components/LocationPicker.jsx
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const LocationPicker = ({ onLocationSelect }) => {
   const [viewport, setViewport] = useState({
     latitude: -38.5545,
     longitude: -58.7396,
-    zoom: 13,
+    zoom: 14,
     width: '100%',
     height: '500px',
     center: [-58.7396, -38.5545],
   });
+
+  const handleLocationSelect = useCallback(
+    (newLocation) => {
+      console.log('Nueva ubicación seleccionada:', newLocation);
+      onLocationSelect(newLocation);
+    },
+    [onLocationSelect]
+  );
 
   useEffect(() => {
     mapboxgl.accessToken = import.meta.env.VITE_REACT_APP_MAPBOX_TOKEN;
@@ -21,26 +29,24 @@ const LocationPicker = ({ onLocationSelect }) => {
       zoom: viewport.zoom,
     });
 
-    const marker = new mapboxgl.Marker(); // Crea un marcador
+    const marker = new mapboxgl.Marker();
 
     map.on('load', () => {
       console.log('Mapa cargado exitosamente.');
-    });
+    });e
 
     map.on('click', (e) => {
       const { lng, lat } = e.lngLat;
-      onLocationSelect({ lat, lng });
-
-      // Mueve el marcador a la nueva ubicación
+      handleLocationSelect({ lat, lng });
       marker.setLngLat([lng, lat]).addTo(map);
     });
 
     return () => {
       map.remove();
     };
-  }, [onLocationSelect, viewport]);
+  }, [handleLocationSelect, viewport]);
 
-  return <div id="map-container" style={{ width: '100%', height: '500px', border: '1px solid red' }} />;
+  return <div id="map-container" style={{ width: '500px', height: '500px', border: '1px solid red' }} />;
 };
 
-export default LocationPicker;
+export default React.memo(LocationPicker);
