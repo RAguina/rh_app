36d111fb39984b8e46d43abc4c463389174bb93a
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-25 21:57:57 -0300

    v0.73.0c

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import Contact from "../pages/Contact"
 
 const Footer = () => {
   return (
-    <div className="from-blue-200 to-blue-600 bg-gradient-to-tl">
+    <div className="from-blue-200 to-blue-600 bg-gradient-to-tl mt-10">
       <hr className="border-t-2"/>  
       <h1 className="text-center text-2xl font-bold">Informacion de contacto</h1>
       <Contact/>
