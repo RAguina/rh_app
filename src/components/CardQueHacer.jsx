072f918e689738ceaf53cc139f4510c4edf4cd7c
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-26 11:30:46 -0300

    v0.73.6b

--- src/components/CardQueHacer.jsx
+++ src/components/CardQueHacer.jsx
@@ -4,7 +4,7 @@ const CardQueHacer = ({ imagen, titulo, descripcion }) => {
     <div className="mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
       <div className="md:flex">
         <div className="md:flex-shrink-0">
-          <img className="mb-10 h-48 w-full object-cover md:w-48" src={imagen} alt={titulo} />
+          <img className="my-10 ml-5 h-48 w-full object-cover md:w-48" src={imagen} alt={titulo} />
           <a href="#" className='ml-5'>Mas informacion</a>
         </div>
         <div className="p-8 sm:w-full">
