2c486640eadfe28d30ff26ab8c014a32c95acf8d
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-25 22:27:24 -0300

    v0.73.1d

--- src/components/Nav2.jsx
+++ src/components/Nav2.jsx
@@ -15,23 +15,23 @@ const Nav2 = ({ isOpaque }) => {
     <div className="dropdown relative">
   <button onClick={toggleDropdown} className="dropbtn mr-10">Menú</button>
   {dropdownOpen && (
-    <div className="dropdown-content absolute right-0 style={{ backgroundColor: 'transparent' }}">
-      <Link to="/" className={`navLinks ${location.pathname === '/' ? 'active' : ''}`}>Inicio</Link>
-      <Link to="/registrarPropiedad" className={`navLinks ${location.pathname === '/registrarPropiedad' ? 'active' : ''}`}>Registrar Propiedad</Link>
-      <Link to="/listadoPropiedades" className={`navLinks ${location.pathname === '/listadoPropiedades' ? 'active' : ''}`}>Listado Propiedades</Link>
-      <Link to="/queHacer" className={`navLinks ${location.pathname === '/queHacer' ? 'active' : ''}`}>Que hacer en Necochea?</Link>
-      <Link to="/registrarUsuario" className={`navLinks ${location.pathname === '/registrarUsuario' ? 'active' : ''}`}>Registrarse</Link>
-      <Link to="/contact" className={`navLinks ${location.pathname === '/contact' ? 'active' : ''}`}>Contactanos</Link>
+    <div className="dropdown-content absolute right-0">
+      <Link to="/" className={`navLinks bg-transparent ${location.pathname === '/' ? 'active' : ''}`}>Inicio</Link>
+      <Link to="/registrarPropiedad" className={`navLinks bg-transparent ${location.pathname === '/registrarPropiedad' ? 'active' : ''}`}>Registrar Propiedad</Link>
+      <Link to="/listadoPropiedades" className={`navLinks bg-transparent ${location.pathname === '/listadoPropiedades' ? 'active' : ''}`}>Listado Propiedades</Link>
+      <Link to="/queHacer" className={`navLinks bg-transparent ${location.pathname === '/queHacer' ? 'active' : ''}`}>Que hacer en Necochea?</Link>
+      <Link to="/registrarUsuario" className={`navLinks bg-transparent ${location.pathname === '/registrarUsuario' ? 'active' : ''}`}>Registrarse</Link>
+      <Link to="/contact" className={`navLinks bg-transparent ${location.pathname === '/contact' ? 'active' : ''}`}>Contactanos</Link>
       {isLoggedIn ? (
         <>
-          <Link to="/" onClick={handleLogout} className={`navLinks2`}>Cerrar Sesión</Link>
-          <Link to="/formUploadImages" className={`navLinks2 ${location.pathname === '/formUploadImages' ? 'active' : ''}`}>Subir Imagenes</Link>
+          <Link to="/" onClick={handleLogout} className={`navLinks2 bg-transparent`}>Cerrar Sesión</Link>
+          <Link to="/formUploadImages" className={`navLinks2 bg-transparent ${location.pathname === '/formUploadImages' ? 'active' : ''}`}>Subir Imagenes</Link>
         </>
       ) : (
-        <Link to="/login" className={`navLinks2 ${location.pathname === '/login' ? 'active' : ''}`}>Iniciar Sesión</Link>
+        <Link to="/login" className={`navLinks2 bg-transparent ${location.pathname === '/login' ? 'active' : ''}`}>Iniciar Sesión</Link>
       )}
       {isAdmin && (
-        <Link to="/adminPanel" className={`navLinks2 ${location.pathname === '/adminPanel' ? 'active' : ''}`}>Admin Panel</Link>
+        <Link to="/adminPanel" className={`navLinks2 bg-transparent ${location.pathname === '/adminPanel' ? 'active' : ''}`}>Admin Panel</Link>
       )}
     </div>
   )}
