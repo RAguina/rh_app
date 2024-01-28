65eb18b7dd629af418d4a36c5ca7daf0773d6f81
RAguina <rodrigoaguinagalde@gmail.com>
2024-01-28 00:28:06 -0300

    v0.74.5

--- src/pages/LocationStep.jsx
+++ src/pages/LocationStep.jsx
@@ -3,11 +3,14 @@ import LocationPicker from '../components/LocationPicker.jsx';
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { agregarCoordenadas } from '../api/inmuebles.js';
+import ErrorPage from '../components/ErrorPage.jsx';
 
 const LocationStep = () => {
   console.log('LocationStep se está renderizando.');
   const {propiedadId} = useParams();
   const [selectedLocation, setSelectedLocation] = useState(null);
+  const [errorMessage, setErrorMessage] = useState({ mensaje: null, tipo: null });
+  const navigate = useNavigate();
 
   const handleConfirmClick = async () => {
     if (selectedLocation) {
@@ -15,11 +18,22 @@ const LocationStep = () => {
       try {
         await agregarCoordenadas(propiedadId, lat, lng);
         console.log('Coordenadas actualizadas exitosamente.');
+        setErrorMessage({ mensaje: 'Las coordenadas se guardaron correctamente. Espera por favor, en instantes serás redireccionado', tipo: 'exito' });
       } catch (error) {
         console.error('Error actualizando las coordenadas:', error);
+        setErrorMessage({ mensaje: 'Hubo un error al guardar las coordenadas. Por favor, inténtalo de nuevo.', tipo: 'error' });
       }
     }
   };
+
+  useEffect(() => {
+    if (errorMessage && errorMessage.tipo === 'exito') {
+      const timer = setTimeout(() => {
+        navigate('/ruta-a-la-que-quieres-redirigir');
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage, navigate]);
 
   return (
     <>
@@ -28,6 +42,7 @@ const LocationStep = () => {
       <div className="mt-8 w-500 h-500">
         <LocationPicker onLocationSelect={setSelectedLocation} />
       </div>
+      {errorMessage && <ErrorPage mensaje={errorMessage.mensaje} tipo={errorMessage.tipo} />}
       {selectedLocation && (
       <button 
       className='navLinks'
